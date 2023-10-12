b5c91bbd5e1c7e3b68a24f282c54637ce0cad794
Ameen <mqtdyh122@gmail.com>
2023-10-12 05:10:05 +0300

    admin is here

--- main.js
+++ main.js
@@ -4,3 +4,15 @@ alert("admin is here ");
 document.write("admin is here ");
 console.log("admin is here !");
 console.log("Hi From Admin ");
+
+// console.log(name);
+alert("admin is here ");
+document.write("admin is here ");
+console.log("admin is here !");
+console.log("Hi From Admin ");
+
+// console.log(name);
+alert("admin is here ");
+document.write("admin is here ");
+console.log("admin is here !");
+console.log("Hi From Admin ");
